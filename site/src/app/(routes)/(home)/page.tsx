22026008105dbce1d285cdc9de6e14404b8dea4c
Rad-Point <clinicaradpoint@gmail.com>
2025-04-24 20:53:18 -0300

    feat: acabou krai

--- site/src/app/(routes)/(home)/page.tsx
+++ site/src/app/(routes)/(home)/page.tsx
@@ -1,26 +1,30 @@
 "use client";
 import { useEffect } from "react";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
 export default function Home() {
-  const click = async () => {
-    const q = await fetch("/api/exames/get", {
-      method: "POST",
-      body: JSON.stringify({ numero: "R-1777854", senha: "R7606697" }),
-    });
-    const res = await q.json();
-    console.log(res);
-  };
   useEffect(() => {}, []);
   return (
-    <main className="w-screen h-screen flex flex-col gap-2 items-center justify-center">
-      <Button
-        onClick={(e) => {
-          e.preventDefault();
-          click();
-        }}
-      >
-        Test
-      </Button>
+    <main className="w-full min-h-screen">
+      <header className="w-full h-16 p-2 flex justify-between items-center border-b border-accent">
+        <Image
+          className="rounded-full object-cover"
+          src="/logo.jpg"
+          alt="logo"
+          width={40}
+          height={40}
+        />
+        <nav>
+          <ul>
+            <li>
+              <Button variant={"link"} asChild>
+                <Link href="/exames/resultado">Resultados</Link>
+              </Button>
+            </li>
+          </ul>
+        </nav>
+      </header>
     </main>
   );
 }
--- site/src/app/(routes)/dashboard/page.tsx
+++ site/src/app/(routes)/dashboard/page.tsx
@@ -1,6 +1,16 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const DashboardPage = () => {
   return (
-    <main className="w-screen h-screen flex flex-col items-center justify-center gap-2"></main>
+    <main className="w-screen h-screen flex flex-col items-center justify-center gap-2">
+      <Button asChild>
+        <Link href="/dashboard/laudos/gerar-laudo">Gerar Laudo</Link>
+      </Button>
+      <Button asChild>
+        <Link href="/dashboard/exames/anexar-imagens">Anexar imagens</Link>
+      </Button>
+    </main>
   );
 };
 
