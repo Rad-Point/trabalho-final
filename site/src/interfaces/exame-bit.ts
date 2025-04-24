da4666e7fa05e778966c9d0fecffef58f277551a
Rad-Point <clinicaradpoint@gmail.com>
2025-04-24 19:04:08 -0300

    bugfix: g

--- site/src/interfaces/exame-bit.ts
+++ site/src/interfaces/exame-bit.ts
@@ -2,4 +2,4 @@ export type ExameBit =
   | { type: "paragraph"; content: string }
   | { type: "image"; content: string; uploaded: boolean };
 
-export const exameTypes = ["paragraph", "image"] as const;
+export type ExameTypes = "paragraph" | "image";
